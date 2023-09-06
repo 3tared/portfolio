d7d66c3f935b97e667ab3813dadd5cc573ebbaa9
3tared <mohamed2011305977@gmail.com>
2023-09-06 03:04:51 +0300

    updating mobile width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
   return (
     <section className=" relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 sm:top-[200px] lg:top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 sm:top-[200px] lg:top-[250px] top-[210px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
